6031b2f5e815fbbe5aabc0ce0257d2310b7efafe
tjallen27 <tjallen91@hotmail.co.uk>
2017-12-16 17:54:11 +0000

    Styling update

--- js/app.js
+++ js/app.js
@@ -2,49 +2,41 @@ const ul = document.getElementById('coins--container');
 const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=100';
 
 
+console.log('function fired!');
+// fetch data from url
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
 
+  const coins = data;
 
-// wrap data in a setInterval function
-function getData(){
-  console.log('function fired!');
-  // fetch data from url
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
+  // for each coin, create a list with properties
+  for(var prop in coins){
+    console.log(coins[prop]);
+    // create new elements for list / sub-lists
+    const liTop = document.createElement('li');
+    const ulBottom = document.createElement('ul');
+    const liOne = document.createElement('li');
+    const liTwo = document.createElement('li');
+    const liThree = document.createElement('li');
 
-    const coins = data;
+    // add a title for top list
+    liTop.className = 'coin_title';
 
-    // for each coin, create a list with properties
-    for(var prop in coins){
+    // append top line data and elements
+    ul.append(liTop);
+    liTop.append(`${coins[prop].id}`);
+    liTop.append(ulBottom);
 
-      // create new elements for list / sub-lists
-      const liTop = document.createElement('li');
-      const ulBottom = document.createElement('ul');
-      const liOne = document.createElement('li');
-      const liTwo = document.createElement('li');
-      const liThree = document.createElement('li');
+    // append sub-list of properties
+    ulBottom.append(liOne, liTwo, liThree);
+    liOne.append(`Price: $${coins[prop].price_usd}`);
+    liTwo.append(`Available: $${coins[prop].available_supply}`);
+    liThree.append(`Market Cap: $${coins[prop].market_cap_usd}`);
+  }
+})
 
-      // add a title for top list
-      liTop.className = 'coin_title';
-
-      // append top line data and elements
-      ul.append(liTop);
-      liTop.append(`${coins[prop].id}`);
-      liTop.append(ulBottom);
-
-      // append sub-list of properties
-      ulBottom.append(liOne, liTwo, liThree);
-      liOne.append(`Price: $${coins[prop].price_usd}`);
-      liTwo.append(`Available: $${coins[prop].available_supply}`);
-      liThree.append(`Market Cap: $${coins[prop].market_cap_usd}`);
-    }
-  })
-
-  // log errors
-  .catch(function(error) {
-    console.log(error);
-  });
-}
-
-// call the function every second to update data
-setInterval(getData, 1000);
+// log errors
+.catch(function(error) {
+  console.log(error);
+});
